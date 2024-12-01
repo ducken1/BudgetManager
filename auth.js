f1960d0606080931feb4cd908ff536a9a7092b86
Erik Lašič <eric.lasic12345@gmail.com>
2024-12-01 12:37:45 +0100

    test commit

--- auth.js
+++ auth.js
@@ -8,7 +8,7 @@ const createToken = (user) => {
         { expiresIn: '1h' }
     );
 };
-
+//test
 const authenticateToken = (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
     if (!token) return res.sendStatus(401);
